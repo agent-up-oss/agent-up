fea82cb69e9cd92aa2c9a1e4424456819d2280c8
Daniel Maß <me@themassiveone.net>
2026-07-21 18:51:45 +0200

    docs: prefer dark mode by default

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -61,6 +61,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        respectPrefersColorScheme: false,
+      },
       image: 'img/social-card.svg',
       navbar: {
         title: 'Agent-Up',
