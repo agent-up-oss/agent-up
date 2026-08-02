9ce8fa1e07e010a71edc9b553ae2cfd3e490d8d3
themassiveone <me@themassiveone.net>
2026-08-02 10:44:37 +0200

    refactor!(Workspaces): remove BrowserUrlStore and polling composition

--- AgentUp.Desktop/Shared/Interfaces/IBrowserWindowHost.cs
+++
@@ -1,8 +0,0 @@
-namespace AgentUp.Desktop.Shared.Interfaces;
-
-internal interface IBrowserWindowHost
-{
-    Task<IReadOnlyCollection<string>> GetActiveWorkspaceIdsAsync();
-    Task<string?> EvalAsync(string workspaceId, string script);
-    bool NavigateTo(string workspaceId, string? url);
-}
