2bbab86e69dda78adf69656d2e80de9674ffbe63
themassiveone <me@themassiveone.net>
2026-08-04 18:49:16 +0200

    ci: add localinstaller.yml workflow with semantic-release using localinstaller-v tag prefix

---
+++ .github/scripts/localinstaller-release.js
@@ -0,0 +1,35 @@
+'use strict';
+
+async function main() {
+  const semanticReleaseModule = require('semantic-release');
+  const semanticRelease = semanticReleaseModule.default || semanticReleaseModule;
+
+  await semanticRelease({
+    tagFormat: 'localinstaller-v${version}',
+    branches: ['main'],
+    plugins: [
+      ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
+      [
+        '@semantic-release/github',
+        {
+          assets: [{ path: 'nupkgs/*.nupkg', label: 'NuGet packages' }],
+        },
+      ],
+      [
+        '@semantic-release/exec',
+        {
+          publishCmd:
+            '[ -z "$NUGET_API_KEY" ] || dotnet nuget push nupkgs/*.nupkg' +
+            ' --api-key "$NUGET_API_KEY"' +
+            ' --source https://api.nuget.org/v3/index.json' +
+            ' --skip-duplicate',
+        },
+      ],
+    ],
+  });
+}
+
+main().catch(err => {
+  console.error(err.message);
+  process.exit(1);
+});
